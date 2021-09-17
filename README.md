3ac4d4cfc088b19f49c9e24b2c8e12181eec98f5
Mohammad Chowdhury <riponusq@yahoo.com>
2021-09-17 14:30:20 +1000

    update ReadMe file

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # code-quiz-homework
+
+This is the homework of CODE QUIZ using hTML, CSS and  javaScript . I tried to make it what I can.
+
+I have made this homework in the following ways: 
+
+I used Array for question elements. 
+when I click the start Button a timer start and I am presented with a question
+When I answer a question then I am presented with another question.
+when all questions are answeres or the time reaches 0 then the game is over.
+I tried to make a score and initials of my score but it didnt come through. 
+
+Credits:
+    Ben Atkinson. (Instructor).
+    Yun Zhao.(TA). [Special thanks to TA (YUN ZHAO), who helped me out of the official hours]
+    W3 schoolwebsite.
+    Bryan (instructor).
+    Rebeca Gonzalez.
--- main.js
+++ main.js
@@ -52,32 +52,44 @@ var time = setInterval(quizTimer, 1000);
 function quizTimer() {
     document.getElementById('time').innerHTML = sec + "sec left";
     sec--;
+// within if statement set in html of the container div to display score
     if (sec == -1) {
+        endGame()
+
         clearInterval(time);
     }
 }
-
+function endGame(){
+    document.getElementById("quiz").innerHTML = "GAME OVER"
+}
 var quiz = document.getElementById("quiz")
 var answerEls = document.querySelectorAll(".answer")
 var questionEls = document.getElementById("question")
 var a_question = document.getElementById("a_question")
+var a = document.getElementById("a")
 var b_question = document.getElementById("b_question")
+var b = document.getElementById("b")
 var c_question = document.getElementById("c_question")
+var c = document.getElementById("c")
 var d_question = document.getElementById("d_question")
+var d = document.getElementById("d")
 var submitBtn = document.getElementById("submit")
 
 var currentQuiz = 0
 var score = 0
-
 function loadQuiz() {
     
     var currentQuizQuestion = quizQuestion[currentQuiz]
 
     questionEls.innerText = currentQuizQuestion.question
     a_question.innerText = currentQuizQuestion.a
+    a.checked=false
     b_question.innerText = currentQuizQuestion.b
+    b.checked=false
     c_question.innerText = currentQuizQuestion.c
+    c.checked=false
     d_question.innerText = currentQuizQuestion.d
+    d.checked = false
 }
 
 loadQuiz()
@@ -86,8 +98,11 @@ function submit(){
        currentQuiz++
        loadQuiz()
        if(currentQuiz >= 5){
-                      
+        doucument.getElementById ("quiz"). innerHTML = 'your score' 
+        score++   
+        endGame()
+                             
         }
-                 
+                
 }
 submitBtn.addEventListener("click",submit);
